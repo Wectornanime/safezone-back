a078e17edad32928da827d8b10640bb56c2bb77b
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-10 09:50:48 -0300

    added nodemon

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/")
+mongoose.connect("mongodb://root:root@localhost:27017/")
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Failed to connect to MongoDB', err));
 
