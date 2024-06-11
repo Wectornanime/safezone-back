f9448b96ae77d78beda5b489a9c337c6c3c00431
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-11 14:20:22 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const UserRoutes = require('./routes/userRouter');
 const ReportRoutes = require('./routes/reportRoutes');
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 // Conectar ao banco de dados MongoDB
--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const reportController = require('../controllers/reportController');
 
 router.post('/report', reportController.createReport);
+router.get('/report', reportController.getReports);
 
 module.exports = router;
